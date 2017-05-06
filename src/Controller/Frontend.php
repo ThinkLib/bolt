47305e46973f50f1bd267962a90c3876d2a8a730
Gawain Lynch <gawain.lynch@gmail.com>
2017-05-06 12:53:28 +0200

    Render maintenance mode template directly, as before() is before other listeners

--- src/Controller/Frontend.php
+++ src/Controller/Frontend.php
@@ -43,7 +43,7 @@ class Frontend extends ConfigurableBase
      *
      * @param Request $request The Symfony Request
      *
-     * @return null|TemplateResponse|RedirectResponse
+     * @return null|Response|RedirectResponse
      */
     public function before(Request $request)
     {
@@ -61,9 +61,11 @@ class Frontend extends ConfigurableBase
         // If we are in maintenance mode and current user is not logged in, show maintenance notice.
         if ($this->getOption('general/maintenance_mode')) {
             if (!$this->isAllowed('maintenance-mode')) {
+                $twig = $this->app['twig'];
                 $template = $this->templateChooser()->maintenance();
-                $response = $this->render($template);
-                $response->setStatusCode(Response::HTTP_SERVICE_UNAVAILABLE);
+
+                $html = $twig->resolveTemplate($template)->render([]);
+                $response = new Response($html, Response::HTTP_SERVICE_UNAVAILABLE);
 
                 return $response;
             }
